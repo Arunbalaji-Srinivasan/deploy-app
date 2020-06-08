3df587b034a76e7918f6ffb01296b509ab3f32b7
Arunbalaji-Srinivasan <srinivasan.ar@husky.neu.edu>
2020-06-07 20:06:02 -0400

    Updates

---
+++ precache-manifest.e4d9047a854c3795364716083bef5470.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f6907426efb5cdf88a5527ae59e8899",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f5fac9a38dd6540ca6b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "83cee09a7ee4f5fc0657",
+    "url": "/static/js/2.ab830d09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab830d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5fac9a38dd6540ca6b",
+    "url": "/static/js/main.88513fd5.chunk.js"
+  },
+  {
+    "revision": "004bdb4e0dffacb5175d",
+    "url": "/static/js/runtime-main.e447d0f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
